364e1452ae1f1676de0d784370da185c83af6d6f
qiqizhouu02 <yz6897@nyu.edu>
2022-01-27 23:17:40 -0500

    infinity scroll

---
+++ js files/explore.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+    $('div').fadeIn(1500).removeClass('hidden');
+    console.log('help');
+});
+
+const items = document.querySelectorAll('.masonry-item');
+
+const active = function(entries) {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('inview2');
+        } else {
+            entry.target.classList.remove('inview2');
+        }
+    });
+}
+const io2 = new IntersectionObserver(active);
+for (let i = 0; i < items.length; i++) {
+    io2.observe(items[i]);
+}
