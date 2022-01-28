d54679f1354a06570465b9180abe21d253d0ed22
qiqizhouu02 <yz6897@nyu.edu>
2022-01-27 22:37:14 -0500

    autoscroll

--- js files/homepage.js
+++ js files/homepage.js
@@ -2,3 +2,16 @@ $(document).ready(function() {
     $('div').fadeIn(1500).removeClass('hidden');
     console.log('help');
 });
+
+var firstsec = document.getElementById('fig1');
+var secondsec = document.getElementById('fig2');
+var lastScrollTop = 0;
+window.onscroll = function() {
+    var st = window.pageYOffset || document.documentElement.scrollTop;
+    if (st > lastScrollTop) {
+        secondsec.scrollIntoView({ behavior: "smooth" });
+    } else {
+        firstsec.scrollIntoView({ behavior: "smooth" });
+    }
+    lastScrollTop = st <= 0 ? 0 : st;
+}
