df07bf53d86ad16c512b46032e7d9b673b84c184
qiqizhouu02 <yz6897@nyu.edu>
2022-01-29 00:42:35 -0500

    got rid of scroll snap but added third work

--- js files/homepage.js
+++ js files/homepage.js
@@ -3,15 +3,19 @@ $(document).ready(function() {
     console.log('help');
 });
 
-var firstsec = document.getElementById('fig1');
-var secondsec = document.getElementById('fig2');
-var lastScrollTop = 0;
-window.onscroll = function() {
-    var st = window.pageYOffset || document.documentElement.scrollTop;
-    if (st > lastScrollTop) {
-        secondsec.scrollIntoView({ behavior: "smooth" });
-    } else {
-        firstsec.scrollIntoView({ behavior: "smooth" });
-    }
-    lastScrollTop = st <= 0 ? 0 : st;
-}
+// var firstsec = document.getElementById('fig1');
+// var secondsec = document.getElementById('fig2');
+// var thirdsec = document.getElementById('fig3');
+// var lastScrollTop = 0;
+// window.onscroll = function() {
+//     var st = window.pageYOffset || document.documentElement.scrollTop;
+//     if (st > seccondsec) {
+//         thirdsec.scrollIntoView({ behaviour: "smooth" });
+//     } else { secondsec.scrollIntoView({ behaviour: "smooth" }) }
+//     if (st > lastScrollTop) {
+//         secondsec.scrollIntoView({ behavior: "smooth" });
+//     } else {
+//         firstsec.scrollIntoView({ behavior: "smooth" });
+//     }
+//     lastScrollTop = st <= 0 ? 0 : st;
+// }
