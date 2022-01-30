6955cead56382c06c480bbb8e01e1f5e63f53a89
qiqizhouu02 <yz6897@nyu.edu>
2022-01-29 22:18:03 -0500

    more changes

--- js files/homepage.js
+++ js files/homepage.js
@@ -3,13 +3,29 @@ $(document).ready(function() {
     console.log('help');
 });
 
+const items = document.querySelectorAll('.figure');
+
+const active = function(entries) {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('viewscroll');
+        } else {
+            entry.target.classList.remove('viewscroll');
+        }
+    });
+}
+const io2 = new IntersectionObserver(active);
+for (let i = 0; i < items.length; i++) {
+    io2.observe(items[i]);
+}
+
 // var firstsec = document.getElementById('fig1');
 // var secondsec = document.getElementById('fig2');
 // var thirdsec = document.getElementById('fig3');
 // var lastScrollTop = 0;
 // window.onscroll = function() {
 //     var st = window.pageYOffset || document.documentElement.scrollTop;
-//     if (st > seccondsec) {
+//     if (st > seccondsec.scrollIntoView) {
 //         thirdsec.scrollIntoView({ behaviour: "smooth" });
 //     } else { secondsec.scrollIntoView({ behaviour: "smooth" }) }
 //     if (st > lastScrollTop) {
