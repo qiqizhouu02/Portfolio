2dbcd2cd2343d195b7acce515006788ab5c29e03
qiqizhouu02 <yz6897@nyu.edu>
2022-02-09 11:15:56 -0500

    adding things for soundify

---
+++ js files/projects.js
@@ -0,0 +1,15 @@
+$('.container').parallaxBackground({
+
+    // 'shift', 'fade'
+    animation_type: 'shift',
+
+    // zoom level
+    // zoom: 20,
+
+    // // rotate perspective
+    // rotate_perspective: 1400,
+
+    // // duration of animation
+    // animate_duration: 1
+
+});
